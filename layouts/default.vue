67534a76ca8fb1dd6de9f9177c786b186cda886c
grzehoo <kazimierski.grzegorz@gmail.com>
2020-09-02 20:09:41 +0200

    inicjalny commit

---
+++ layouts/default.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-app light>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <nuxt-link to="/">{{ title }}</nuxt-link>
+      <v-spacer />
+      <v-btn icon @click.stop="openSearch">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="openBasket">
+        <v-icon>mdi-basket</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      :width="'300px'"
+      temporary
+      fixed
+    >
+      <SearchAutocomplete v-show="!showBasket" />
+      <v-list v-show="showBasket">
+        <v-list-item to="/basket">
+          <v-list-item-title>Koszyk</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import SearchAutocomplete from '../components/SearchAutomplete'
+
+export default {
+  components: {
+    SearchAutocomplete,
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      showBasket: false,
+      title: 'Standard Vue',
+    }
+  },
+  methods: {
+    openSearch() {
+      this.rightDrawer = !this.rightDrawer
+      this.showBasket = false
+    },
+    openBasket() {
+      this.rightDrawer = !this.rightDrawer
+      this.showBasket = true
+    },
+  },
+}
+</script>
